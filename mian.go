2226afb016ed2ac289054c184ac00bca5f125811
缪宇 <miaoyu2009@qq.com>
2023-08-22 12:04:30 +0800

    feat: init env

--- mian.go
+++ mian.go
@@ -1,7 +1,24 @@
 package main
 
-import "fmt"
+import (
+	"gologin/inits"
+
+	"github.com/gin-gonic/gin"
+)
+
+// this init function will be called automatically and cannot have any parameters
+func init() {
+	inits.LoadEnv()
+}
 
 func main() {
-	fmt.Println("Hello, World!")
+	r := gin.Default()
+
+	r.GET("/", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "hello world",
+		})
+	})
+
+	r.Run()
 }
