b6609e9f889961207ec8a11525eaa15b99b0dffb
缪宇 <miaoyu2009@qq.com>
2023-09-04 11:42:08 +0800

    feat: email validate

--- vo/user.go
+++ vo/user.go
@@ -1,6 +1,6 @@
 package vo
 
 type LoginVo struct {
-	Email    string `json:"email" validate:"required" label:"邮件" field_error_info:"邮件不能为空"`
+	Email    string `json:"email" validate:"required,email" label:"邮件" field_error_info:"邮件不能为空"`
 	Password string `json:"password" validate:"required" label:"密码" `
 }
